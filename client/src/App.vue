4e3cdac5ca731747b1942a85e5fe92226e69aa81
sh1v-ansh <shivanshsoni321@gmail.com>
2024-10-28 09:51:10 -0400

    demo

--- client/src/App.vue
+++ client/src/App.vue
@@ -42,7 +42,8 @@ Pages:
       return {
         recipes: [
           { id: 1, name: 'Pasta', description: 'Classic pasta.', ingredients: ['pasta', 'tomato', 'cheese'], tags: ["Gluten Free", "Vegetarian"] },
-          { id: 2, name: 'Salad', description: 'Healthy salad.', ingredients: ['lettuce', 'tomato', 'cucumber'], tags: ["Vegan"] }
+          { id: 2, name: 'Salad', description: 'Healthy salad.', ingredients: ['lettuce', 'tomato', 'cucumber'], tags: ["Vegan"] },
+          { id: 2, name: 'Chicken Pot Pie', description: 'Delicious and meaty', ingredients: ['chicken', 'sauce', 'vegetables'], tags: ["Protein", "High Calorie"] }
         ],
         filter: ''
       };
