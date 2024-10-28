0d0b867672b3caea9d9954fcfb473c72b384b7f3
sh1v-ansh <shivanshsoni321@gmail.com>
2024-10-28 09:47:50 -0400

    added filtering based on recipe tags

---
+++ client/src/App.vue
@@ -0,0 +1,63 @@
+/**
+
+Components to create:
+
+- Navbar
+- Recipe Card
+- Description Card
+- Filtered Recipe Menu
+- Footer
+
+*/
+
+/**
+
+Pages:
+
+- Home Page (Explore)
+- Recipes Page
+- Specific Recipe Page
+- Profile Page
+
+*/
+
+<template>
+  <div>
+    <h2>Recipe List</h2>
+    <RecipeFilter @update-filter="filterRecipes" />
+    <div v-for="recipe in filteredRecipes" :key="recipe.id">
+      <Recipe :recipe="recipe" />
+    </div>
+  </div>
+</template>
+
+<script>
+  import Recipe from './components/Recipe.vue';
+  import RecipeFilter from './components/Filter.vue';
+
+  export default {
+    components: { Recipe, RecipeFilter },
+
+    data() {
+      return {
+        recipes: [
+          { id: 1, name: 'Pasta', description: 'Classic pasta.', ingredients: ['pasta', 'tomato', 'cheese'], tags: ["Gluten Free", "Vegetarian"] },
+          { id: 2, name: 'Salad', description: 'Healthy salad.', ingredients: ['lettuce', 'tomato', 'cucumber'], tags: ["Vegan"] }
+        ],
+        filter: ''
+      };
+    },
+    computed: {
+      filteredRecipes() {
+        return this.recipes.filter(recipe =>
+          recipe.tags.some(tag => tag.toLowerCase().includes(this.filter.toLowerCase()))
+        );
+      }
+    },
+    methods: {
+      filterRecipes(keyword) {
+        this.filter = keyword;
+      }
+    }
+  };
+</script>
---
+++ client/src/components/Filter.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <label>Filter by keyword:</label>
+        <input v-model="keyword" @input="$emit('update-filter', keyword)" />
+    </div>
+</template>
+  
+<script>
+    export default {
+        data() {
+            return {
+            keyword: ''
+            };
+        }
+    }
+</script>
+  
