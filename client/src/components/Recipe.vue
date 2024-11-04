78a2ed7bf106ec6f0f9d18b255105878c40e2d02
dtrain0r <dtrainor@umass.edu>
2024-11-04 10:59:54 -0500

    Put shivansh's code into new client folder

---
+++ client/src/components/Recipe.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="recipe-card">
+    <h3>{{ recipe.name }}</h3>
+    <p>{{ recipe.description }}</p>
+    <p>Ingredients: {{ recipe.ingredients.join(', ') }}</p>
+    <p>Tags: {{ recipe.tags.join(', ') }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      recipe: Object
+    }
+  }
+</script>
+
+<style scoped>
+.recipe-card {
+  border: 1px solid #000000;
+  border-radius: 25px;
+  padding: 1em;
+  margin: 0.5em 0;
+  box-shadow: 3px 3px 3px gray;
+}
+</style>
