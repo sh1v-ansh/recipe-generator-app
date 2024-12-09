477d1728f1c39746539ad40206db6183f10b7033
dtrain0r <dtrainor@umass.edu>
2024-12-09 00:34:03 -0500

    updates to maintaing auth in local storage in front end
    updates to users and fetchng based on user data
    in back end

--- client/src/Preferences.vue
+++ client/src/Preferences.vue
@@ -119,13 +119,7 @@ const submitPreferences = async () => {
   for (const [category, tags] of Object.entries(preferences)) {
     Object.keys(tags).forEach((tag) => {
       if (tags[tag]) {
-        if(allergens.includes(tag)){
-          selectedPreferences[tag.toLowerCase()] = false;
-        }
-        else{
-          selectedPreferences[tag.toLowerCase()] = true;
-        }
-        
+        selectedPreferences[tag.toLowerCase()]= true; 
       }
     });
   }
